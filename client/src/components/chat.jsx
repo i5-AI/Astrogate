5ed221c02d42e9dcb83ca5a9086c130ddefe73a5
sophia pung <sophiagracepung@gmail.com>
2024-04-17 10:50:34 -0500

    user interface chat

--- client/src/components/chat.jsx
+++ client/src/components/chat.jsx
@@ -1,23 +1,81 @@
 import '../../style.css'
 import './arrow.css'
+import { Component } from 'preact'; 
 
-function Chat() {
-    return (
+export default class Chat extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          userInput: '',
+          messages: []
+      };
+      this.userInputRef = null;
+  }
+
+  handleSubmit = () => {
+    const userInputTrimmed = this.userInputRef.value.trim();
+    if (userInputTrimmed) {
+      this.setState(prevState => ({
+        messages: [...prevState.messages, userInputTrimmed],
+        userInput: ''
+      }), () => {
+        this.userInputRef.value = '';
+      });
+    } else {
+      console.log('No query to send.');
+    }
+  };  
+
+  handleInputChange = (e) => {
+    if (e.key === 'Enter') {
+      this.handleSubmit();
+    } else {
+      this.setState({ userInput: e.target.value });
+    }
+  };
+
+  scrollToBottom = () => {
+    const chatContainer = document.querySelector('.user-chats');
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.messages.length < this.state.messages.length) {
+      this.scrollToBottom();
+    }
+  }
+
+render() {
+  return (
     <div className="chat">
       <div className="chat-title">
         <p>Astrogate</p>
       </div>
       <div className="chats-container">
         <div className="astrogate-chats"></div>
-        <div className="user-chats"></div>
+        <div className="user-chats">
+          {this.state.messages.map((message, index) => (
+          <div key={index} className="chat-bubble">
+            {message}
+          </div>
+            ))}
+        </div>
       </div>
       <div className="messages">
         <div className="message-bar">
-            <input className="input-text" type="text" placeholder="Type a message... "></input>
+        <input
+          ref={el => this.userInputRef = el}
+          className="input-text"
+          type="text"
+          placeholder="Type a message..."
+          value={this.state.userInput} 
+          onChange={this.handleInputChange} 
+          onKeyPress={this.handleInputChange}
+        />
         </div>
-        <button className="submit-message">
+        <button className="submit-message" onClick={this.handleSubmit}>
           <div class="container2">
-            <div class="arrow-and-circle">
+            <div id="submit" class="arrow-and-circle">
               <div class="icono-arrow1-up arrow"></div>
               <div class="circle"></div>
             </div>
@@ -25,7 +83,6 @@ function Chat() {
         </button>
       </div>
     </div>
-    );
+        );
+      }
   }
-
-  export default Chat;
--- client/src/script.js
+++ client/src/script.js
@@ -2,7 +2,6 @@ document.querySelector('.submit-message').addEventListener('mousedown', function
     this.classList.add('clicked');
   });
   
-  document.querySelector('.submit-message').addEventListener('mouseup', function() {
-    this.classList.remove('clicked');
-  });
-  
+document.querySelector('.submit-message').addEventListener('mouseup', function() {
+  this.classList.remove('clicked');
+});
